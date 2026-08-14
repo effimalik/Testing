0ccd2e56c061de0ce091e7f690073560fe48a8f4
Afan Haidar <57601815+effimalik@users.noreply.github.com>
2026-08-14 18:01:45 +0400

    Update dataLayer.js

--- shared/dataLayer.js
+++ shared/dataLayer.js
@@ -42,7 +42,7 @@
      CONFIG — must match auth.js API_BASE
   ───────────────────────────────────────── */
   // const API_BASE = 'https://script.google.com/macros/s/AKfycbw7xlhR-LtEU43aW_2QNMz4BFcIQnFjB2BHkvVyk-arYGXPe0AmBatF8CHBOzv0OM8/exec';
-   const API_BASE = 'https://script.google.com/macros/s/AKfycbwXkQRxr7BJjS50nm30jxoQxts7zwHws_s3Ij2bNS_C7ba3KyQ1cnrsDqiNebhfmWKL/exec';
+   const API_BASE = 'https://script.google.com/macros/s/AKfycbwrHCi1-Ur0L3Rns8sAPNVLkRH4UIXAxt3t6tQB-rnjtBDIMZPzGf5PNCOwaDMZYClI/exec';
 
   const CACHE_PREFIX = 'ap2_';
 
