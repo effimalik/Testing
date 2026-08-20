05364cd03aefaacb9befefa5178cc01589c66734
Afan Haidar <57601815+effimalik@users.noreply.github.com>
2026-08-21 00:08:31 +0400

    Update auth.js

--- shared/auth.js
+++ shared/auth.js
@@ -39,7 +39,7 @@
   const SERVER_CHECK_INT = 5  * 60 * 1000;    // server ping every 5 min
   const ALLOWED_ORIGIN   = 'https://effimalik.github.io/Testing/';
  // const API_BASE = 'https://script.google.com/macros/s/AKfycbwrHCi1-Ur0L3Rns8sAPNVLkRH4UIXAxt3t6tQB-rnjtBDIMZPzGf5PNCOwaDMZYClI/exec';
-   const API_BASE = 'https://script.google.com/macros/s/AKfycbxo0_uKFSZKzbrn-Oh6dllVLskmheehI8ZhJeO8IHNTNnH-fIUNSaOoKM5uE4WC6ajB/exec';
+   const API_BASE = 'https://script.google.com/macros/s/AKfycbxYFAhv4ov32IF5yWczp6x9SDcBfirkoTzT803iYJGSfFa0gAoFmD8CbV5lv0ntF5Sg/exec';
 
      
 
