49932e2924f02503646996d129ac68acc8f50ed8
Afan Haidar <57601815+effimalik@users.noreply.github.com>
2026-09-24 03:23:21 +0400

    Update apiConfig.js

--- shared/apiConfig.js
+++ shared/apiConfig.js
@@ -1,1 +1,2 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbxbKmd9tS5p5nRoWdcSvysmdF_xk-PxjwUo-suEpFtObmC5h2QTOhFhmXj6mtp8xKBB/exec';
+// const API_URL = 'https://script.google.com/macros/s/AKfycbxbKmd9tS5p5nRoWdcSvysmdF_xk-PxjwUo-suEpFtObmC5h2QTOhFhmXj6mtp8xKBB/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwRY_e7Zvd_EjCzb7oAw98wc820WAKwTA64IFlzf3h5RmGY07fgUUe8P3FzeW47ha73/exec';
